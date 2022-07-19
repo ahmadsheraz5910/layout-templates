d0c7d45078bc9cb398e7508216ca00374b606ef5
devnerd591 <devnerd591@gmail.com>
2022-07-19 19:55:29 +0500

    header iteration 2 completed

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, IconButton, Stack, Toolbar, Typography } from '@mui/material'
+import { Avatar, Box, Divider, IconButton, ListItem, Menu, MenuItem, Stack, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import MenuIcon from "../../public/assets/icons/menu-alt-2.svg";
 import Logo from "../../public/assets/sm-text-logo-red.svg";
@@ -7,30 +7,50 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 type Props = {}
 
+const settings = ['Your Profile', 'Settings', 'Sign Out'];
+const NavLinks = [
+  {
+    label:"Dashboard",
+    href:"/"
+  },
+  {
+    label:"Test Prep",
+    href:"/test-prep"
+  },
+  {
+    label:"About SSAT",
+    href:"/about?query=ssat"
+  },
+  {
+    label:"About Dragon Prep",
+    href:"/about?query=dragonPrep"
+  },
+  {
+    label:"Contact Us",
+    href:"/contact"
+  }
+]
+
 const Header = (props: Props) => {
   const router = useRouter()
-  const NavLinks = [
-    {
-      label:"Dashboard",
-      href:"/"
-    },
-    {
-      label:"Test Prep",
-      href:"/test-prep"
-    },
-    {
-      label:"About SSAT",
-      href:"/about?query=ssat"
-    },
-    {
-      label:"About Dragon Prep",
-      href:"/about?query=dragonPrep"
-    },
-    {
-      label:"Contact Us",
-      href:"/contact"
-    }
-  ]
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  
   return (
     <Box component = "header">
         <Toolbar>
@@ -50,7 +70,45 @@ const Header = (props: Props) => {
               </Link>
             )}
           </Stack>
-          <Avatar alt="Remy Sharp" src = {'assets/test-avatar.png'} />
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src = {'assets/test-avatar.png'} />
+            </IconButton>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <ListItem>
+                <Typography variant = "body1">
+                  {`Edward`}<br></br>{`edward@gmail.com`}
+                </Typography>
+              </ListItem>
+              <Divider />
+              <Box sx = {{pt:1, pb:1, minWidth:"200px"}}>
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography component = "a" textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Box>
+              <Divider />
+              <MenuItem>
+                <Typography component = "a">{`Delete`}</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
         </Toolbar>    
     </Box>
   )
